f1c78c7194a354279c8538457f8f97b546686db4
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2023-12-14 05:09:26 +0000

    feat: add husky & lint-staged setup

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '{apps,libs,tools}/**/*.{ts}': files => {
+        return `nx affected --target=typecheck --files=${files.join(',')}`;
+    },
+    '{apps,libs,tools}/**/*.{js,ts,json,html,scss}': [
+        files => `nx affected:lint --files=${files.join(',')}`,
+        files => `nx format:write --files=${files.join(',')}`,
+    ],
+};
