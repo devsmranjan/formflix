5c17ea34d02d2b97a5b222ab6d6d5fc7eafbfd29
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2023-12-14 05:39:21 +0000

    style: format code with new prettier config

--- apps/formflix/src/app/app.component.ts
+++ apps/formflix/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule],
-  selector: 'formflix-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    imports: [RouterModule],
+    selector: 'formflix-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'formflix';
+    title = 'formflix';
 }
